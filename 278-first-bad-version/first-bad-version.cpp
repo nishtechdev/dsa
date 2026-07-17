e9170b5bc1cb0599411e2831287ee8dcbf76cf0d
Nishant Mishra <nishant.1si23cs122@gmail.com>
2026-07-17 14:19:47 +0530

    Time: 3 ms (16.43%) | Memory: 8 MB (4.93%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -4,14 +4,14 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int ans=-1;
+        //int ans=-1;
         int left=1,right=n;
         while(left<=right)
         {
             int mid=left+(right-left)/2;
             if(isBadVersion(mid))
             {
-                ans=mid;
+                //ans=mid;
                 right=mid-1;
             }
             else
@@ -19,7 +19,7 @@ public:
                 left=mid+1;
             }
         }
-        return ans;
+        return left;
         
     }
 };
